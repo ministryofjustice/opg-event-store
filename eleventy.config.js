c9deb1285f6733883cca45a8c087438f075069f2
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2025-06-06 12:07:45 +0100

    Show all examples on event page
    Include all JSON files in the event's `examples` directory on the event page rather than just looking for `example.json`.
    
    Also fix asset passthrough from govuk-frontend so images work, but change the font-family so we don't use Transport on an invalid domain.
    
    And move the `opg.digital.metrics` definition into the right folder.
    
    #patch

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import markdownit from './src/_includes/util/markdown-it.js'
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import { HtmlBasePlugin } from "@11ty/eleventy";
@@ -8,33 +9,42 @@ export default async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ 'src/_includes/css': 'assets' })
     eleventyConfig.addPassthroughCopy({ 'src/_includes/assets': 'assets' })
     eleventyConfig.addPassthroughCopy({ 'src/_includes/scripts': 'assets' });
+    eleventyConfig.addPassthroughCopy({ 'node_modules/govuk-frontend/dist/govuk/assets': 'assets' });
 
     eleventyConfig.addPlugin(syntaxHighlight);
-    
+
     //Pull in path prefix if building for GH Pages
     let path = process.env.BASE_PATH || "/";
 
     console.log('basepath set to '+path)
     //TODO remove when moved to org url
     eleventyConfig.addPlugin(HtmlBasePlugin, {
-      baseHref: path 
+      baseHref: path
     });
-
-    
 
     //switch to GDS markdown by default
     eleventyConfig.setLibrary("md", markdownit());
 
-    //filter for cases not covered by above 
+    //filter for cases not covered by above
     eleventyConfig.addFilter("markdownit", function(value) { return markdownit().render(value || '') });
-    
 
-    eleventyConfig.addFilter("exampleFile", function(value) { 
-      let slash= value.lastIndexOf('/')
-      return '../../' + value.substring(0, slash) + '/examples/example.json'
+    eleventyConfig.addFilter("listExampleFiles", function (partialPath) {
+      let slash = partialPath.lastIndexOf("/");
+
+      const fullPath = "./src/" + partialPath.substring(0, slash) + "/examples";
+      if (!fs.existsSync(fullPath)) {
+        return [];
+      }
+
+      return fs.readdirSync(fullPath).filter(x => x.endsWith('.json'));
     });
 
-    eleventyConfig.addFilter("schemaFile", function(value) { 
+    eleventyConfig.addFilter("exampleFile", function(value, filename='example.json') {
+      let slash= value.lastIndexOf('/')
+      return '../../' + value.substring(0, slash) + '/examples/' + filename
+    });
+
+    eleventyConfig.addFilter("schemaFile", function(value) {
       let slash= value.lastIndexOf('/')
       return '../../' + value.substring(0, slash) + '/schema.json'
     });
@@ -64,7 +74,7 @@ export default async function(eleventyConfig) {
           // The folder where all your content will live:
           input: 'src',
           output: 'out',
-          data: '_data' 
+          data: '_data'
         }
       }
 
